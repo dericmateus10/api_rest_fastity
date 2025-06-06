019a09fb773723521b2be0a003496447fee374a3
Deric Mateus <dericmateus10@gmail.com>
2025-06-06 11:35:09 -0300

    Variáveis ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,14 @@
+import "dotenv/config";
 import { knex as setupKnex } from "knex";
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in the environment variables.");}
+    
 
 export const config = {
     client: "sqlite3",
     connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL,
     },
     useNullAsDefault: true,
     pool: {
