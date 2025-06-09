e090b28dfb548bff5306e6bbd4dd46c2761e710c
Deric Mateus <dericmateus10@gmail.com>
2025-06-08 21:41:19 -0300

    Criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,14 +1,27 @@
 import { FastifyInstance } from 'fastify';
+import { randomUUID } from 'node:crypto';
+import { z } from 'zod';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-app.get("/", async () => {
-  const transactions = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Test Transaction 2',
-    amount: 100.00,
-  }).returning(['*']);
-  return  transactions;
+app.post("/", async (request, reply) => {
+    const createTransactionsBodySchema = z.object({
+        title: z.string().min(1),
+        type: z.enum(['credit', 'debit']),
+        amount: z.number(),
+        
+    });
+    const {title, amount, type} = createTransactionsBodySchema.parse(request.body);
+    await knex('transactions')
+    .insert({
+        id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : -amount,
+        created_at: new Date(),
+    })
+
+
+  return  reply.status(201).send();
 }
 );
 }
