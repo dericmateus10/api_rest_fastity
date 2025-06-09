2c4793c20932b6e404ad05515e598a34d48197d9
Deric Mateus <dericmateus10@gmail.com>
2025-06-09 11:16:57 -0300

    Criando primeiro teste

---
+++ test/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+
+test('O usurio consegue criar uma nova transacao', () => {
+    const responseStatus = 201
+1
+    expect(responseStatus).toEqual(201);
+}); 
+
