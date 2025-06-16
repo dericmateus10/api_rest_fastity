cc4c6975bd02fca53d64666eb6fd391cc627719e
Deric Mateus <dericmateus10@gmail.com>
2025-06-16 12:11:40 -0300

    Preparando para deploy

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [
+    'src/app.ts',
+    'src/database.ts',
+    'src/server.ts',
+    'src/@types/knex.d.ts',
+    'src/env/index.ts',
+    'src/middlewares/check-session-id-exists.ts',
+    'src/routes/transactions.ts',
+  ],
+  target: 'es2016',
+  format: ['cjs'],
+  external: [
+    'pg',
+    'pg-query-stream',
+    'mysql',
+    'mysql2',
+    'oracledb',
+    'better-sqlite3',
+    'tedious'
+  ],
+});
